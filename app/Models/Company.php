b3e54fb859bf31d0653afb7790aced706210add5
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-21 10:36:31 -0600

    Canoe | Add relationships to Models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function funds()
+    {
+        return $this->belongsToMany(Fund::class, 'fund_company', 'company_id', 'fund_id');
+    }
+
 }
--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -1,11 +1,21 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fund extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name', 'start_year', 'manager_id', 'aliases'];
+
+    public function manager()
+    {
+        return $this->belongsTo(FundManager::class, 'manager_id');
+    }
+
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class, 'fund_company', 'fund_id', 'company_id');
+    }
+
 }
