f714cf94906251d970f680fcc75c637f726c32c5
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-22 16:42:48 -0600

    Canoe | Create helper method to validate duplicates and dispatch event

--- app/Services/FundService.php
+++ app/Services/FundService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Events\DuplicateFundWarningEvent;
 use App\Http\Controllers\Requests\FundPostRequest;
+use App\Listeners\HandleDuplicateFundWarningListener;
 use App\Models\Fund;
 use App\Models\FundManager;
 
@@ -11,14 +13,25 @@ class FundService
 
     public function createFund(FundPostRequest $request)
     {
-        $manager = $this->createManager($request->manager);
+        $fundName      = $request->input('fund');
+        $managerName   = $request->input('manager');
 
+        // Check for duplicate
+        $duplicateResult = $this->isDuplicateFund($fundName, $managerName);
+
+        // Dispatch event
+        if ($duplicateResult['isDuplicateFund']) {
+            event(new DuplicateFundWarningEvent($fundName, $managerName, $duplicateResult));
+
+        }
+
+        $manager = $this->createManager($request->manager);
 
         $fund = new Fund();
         $fund->name       = $request->fund;
         $fund->manager_id = $manager->id;
         $fund->start_year = $request->year;
-        $fund->aliases    = $request->aliases;
+        $fund->aliases    = ($request->alias);
         $fund->save();
 
         return $fund;
@@ -26,12 +39,62 @@ class FundService
 
     private function createManager($managerName)
     {
-        //todo check for existing maanger
-        //$manager = FundManager::where('name', $managerName)->first();
         $manager = new FundManager();
         $manager->name = $managerName;
         $manager->save();
 
         return $manager;
     }
+
+    private function isDuplicateFund($fundName, $managerName)
+    {
+        $result = [
+            'isDuplicateFund' => false,
+            'matchAlias'      => null,
+            'matchFundName'     => null,
+            ];
+
+        // Check if a fund with the same name or alias exists for the same manager
+        $existingFund = Fund::where(function ($query) use ($fundName) {
+            $query->where('name', $fundName)
+                ->orWhereJsonContains('aliases', $fundName);
+        })
+            ->whereHas('manager', function ($query) use ($managerName) {
+                $query->where('name', $managerName);
+            })
+            ->get();
+
+        if ($existingFund->count() > 0) {
+            $result['isDuplicateFund'] = true;
+
+            // Check if both fund name and alias match
+            $nameMatch = $existingFund->contains('name', $fundName);
+            $aliasMatch = $existingFund->contains(function ($fund) use ($fundName) {
+                return in_array($fundName, $fund->aliases);
+            });
+
+            if ($nameMatch && $aliasMatch) {
+                $result['matchAlias']    = $fundName;
+                $result['matchFundName'] = $fundName;
+            } elseif ($nameMatch) {
+
+                $result['matchFundName'] = $fundName;
+            } elseif ($aliasMatch) {
+                $matchingFund = $existingFund->where('aliases', 'LIKE', '%"' . $fundName . '"%')->first();
+                $result['matchAlias'] = $matchingFund->aliases;
+            }
+        }
+
+        return $result;
+    }
+
+
+
+
+
+
 }
+
+
+
+//If the incoming fund name matches an existing fund name or a fund alias of an existing manager with the same incoming manager
