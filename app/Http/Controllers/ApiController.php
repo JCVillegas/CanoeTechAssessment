9c61e9e9afb2927087a2b93b8ae90e8e55360df4
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-22 13:22:55 -0600

    Canoe | Inject FundService in construct

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Requests\FundPostRequest;
+use App\Models\FundManager;
 use App\Services\FundService;
 use Illuminate\Http\JsonResponse;
 
 class ApiController
 {
+    protected FundService $fundService;
+
+    public function __construct(FundService $fundService)
+    {
+        $this->fundService = $fundService;
+    }
     public function createFund(FundPostRequest $request): JsonResponse
     {
+        $fund = $this->fundService->createFund($request);
 
-        (new FundService())->createFund($request);
         return response()->json(
             [
                 'success' => true,
-                'message' => 'Fund has been added.'
+                'message' => 'Fund has been created successfully.',
+                'data' => $fund
             ],
             200
         );
