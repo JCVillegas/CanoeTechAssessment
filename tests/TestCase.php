beaea346ce937ee9cf75b29b5af4ab28227490bc
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-23 14:07:17 -0600

    Canoe | Run seeder

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run the seeders before each test
+        $this->seed();
+    }
 }
