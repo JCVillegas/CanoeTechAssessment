034c1c140caeb37159029d76ac683791c3c4cc63
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-23 14:45:46 -0600

    Canoe | Fix the manager id that was missing

--- database/seeders/FundSeeder.php
+++ database/seeders/FundSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Fund;
+use App\Models\FundManager;
 use Illuminate\Database\Seeder;
 
 class FundSeeder extends Seeder
@@ -12,18 +13,21 @@ class FundSeeder extends Seeder
      */
     public function run(): void
     {
+        $manager1 = FundManager::where('name', 'Manager Test 1')->firstOrFail();
+
         Fund::create([
             'name'       => 'Sample Fund 1',
             'start_year' => 2000,
-            'manager'    => 'Manager Test',
-            'alias'      => ['Alias1', 'Alias2', 'Alias3']
+            'manager_id' => $manager1->id,
+            'alias'      => json_encode(['Alias1', 'Alias2', 'Alias3'])
         ]);
 
+        $manager2 = FundManager::where('name', 'Manager Test 2')->firstOrFail();
         Fund::create([
             'name'       => 'Sample Fund 2',
             'start_year' => 2023,
-            'manager'    => 'Manager Test2',
-            'alias'      => ['Alias4', 'Alias5', 'Alias6']
+            'manager_id' => $manager2->id,
+            'alias'      => json_encode(['Alias4', 'Alias5', 'Alias6'])
 
         ]);
     }
