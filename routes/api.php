58003cc2bfe9a73c640d22c94bae55c9b121548f
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-23 07:11:00 -0600

    Canoe | Create route  for read Fund

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(APiFundController::class)->group(function () {
     Route::post('funds', 'createFund');
+    Route::get('funds',  'readFund');
     Route::put('/funds/{id}',  'updateFund');
+
 
 });
