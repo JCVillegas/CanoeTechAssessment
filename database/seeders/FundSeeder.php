1e4b2ad292394b5a48fb1416abb555efca2fa313
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-23 14:47:27 -0600

    Canoe | Fix typo

--- database/seeders/FundSeeder.php
+++ database/seeders/FundSeeder.php
@@ -16,18 +16,18 @@ class FundSeeder extends Seeder
         $manager1 = FundManager::where('name', 'Manager Test 1')->firstOrFail();
 
         Fund::create([
-            'name'       => 'Sample Fund 1',
-            'start_year' => 2000,
-            'manager_id' => $manager1->id,
-            'alias'      => json_encode(['Alias1', 'Alias2', 'Alias3'])
+            'name'         => 'Sample Fund 1',
+            'start_year'   => 2000,
+            'manager_id'   => $manager1->id,
+            'aliases'      => json_encode(['Alias1', 'Alias2', 'Alias3'])
         ]);
 
         $manager2 = FundManager::where('name', 'Manager Test 2')->firstOrFail();
         Fund::create([
-            'name'       => 'Sample Fund 2',
-            'start_year' => 2023,
-            'manager_id' => $manager2->id,
-            'alias'      => json_encode(['Alias4', 'Alias5', 'Alias6'])
+            'name'         => 'Sample Fund 2',
+            'start_year'   => 2023,
+            'manager_id'   => $manager2->id,
+            'aliases'      => json_encode(['Alias4', 'Alias5', 'Alias6'])
 
         ]);
     }
