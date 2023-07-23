89333fe17d0aabdb3125329d1fe5c9a648c0541b
Juan Villegas <juancarlos.villegas@gmail.com>
2023-07-23 15:33:59 -0600

    Canoe | Add update test

--- tests/Unit/Controllers/ApiFundControllerTest.php
+++ tests/Unit/Controllers/ApiFundControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Controllers;
 
+use App\Models\Fund;
 use Database\Seeders\FundManagerSeeder;
 use Database\Seeders\FundSeeder;
 use Tests\TestCase;
@@ -16,10 +17,10 @@ class ApiFundControllerTest extends TestCase
     public function testCreateFund()
     {
         $data = [
-            'fund'      => 'Test Fund',
-            'manager'   => 'Test Manager',
-            'year'      => '2023',
-            'aliases'   => ['alias1', 'alias2'],
+            'fund' => 'Test Fund',
+            'manager' => 'Test Manager',
+            'year' => '2023',
+            'aliases' => ['alias1', 'alias2'],
         ];
 
         $response = $this->postJson('/api/funds', $data);
@@ -27,7 +28,7 @@ class ApiFundControllerTest extends TestCase
         $response->assertStatus(200)
             ->assertJson([
                 'success' => true,
-                ]);
+            ]);
     }
 
     public function testReadFund()
@@ -36,23 +37,52 @@ class ApiFundControllerTest extends TestCase
         $this->seed(FundSeeder::class);
 
 
-       $response = $this->get('/api/funds');
-       $response->assertStatus(200);
-       $response->assertJsonStructure([
+        $response = $this->get('/api/funds');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
             'success',
             'data',
         ]);
 
         $responseData = $response->json();
         $this->assertArrayHasKey('data', $responseData);
-
     }
 
     public function testUpdateFund()
     {
+        $this->seed(FundManagerSeeder::class);
+        $this->seed(FundSeeder::class);
 
+        $fund = Fund::inRandomOrder()->first();
+
+        // Use to update fund
+        $newData = [
+            'fund' => 'Updated Fund',
+            'manager' => 'Manager Test 1',
+            'year' => 2025,
+            'aliases' => ['Alias4', 'Alias5'],
+        ];
+
+        $response = $this->put('/api/funds/' . $fund->id, $newData);
+
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'message' => 'Fund has been updated successfully.',
+                'data' => [
+                    'name' => 'Updated Fund',
+                    'start_year' => 2025,
+                    'aliases' => json_encode(['Alias4', 'Alias5']),
+                ],
+            ]);
+
+        $this->assertDatabaseHas('funds', [
+            'id' => $fund->id,
+            'name' => 'Updated Fund',
+            'start_year' => 2025,
+            'aliases' => json_encode(['Alias4', 'Alias5']),
+        ]);
     }
-
-
 }
 
